c5e7d97812ea2cfc1095da3c678bd549ea154fe9
kaushikhande <kaushikhande302@gmail.com>
2020-01-23 08:49:31 +0530

    run before example

---
+++ before_example_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec/expectations'
+
+class Thing
+  def widgets
+    @widgets ||= []
+  end
+end
+
+RSpec.describe Thing do
+  before(:example) do
+    @thing = Thing.new
+  end
+
+  describe 'intialized in before(:example)' do
+    it 'has 0 widgets' do
+      expect(@thing.widgets.count).to eq(0)
+    end
+
+    it 'can accept new widgets' do
+      @thing.widgets << Object.new
+    end
+
+    it 'does not share state across examples' do
+      expect(@thing.widgets.count).to eq(0)
+    end
+  end
+end
+
